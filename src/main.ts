2833aec942d2080bf9beecded35d1a431343295d
mrchar <mrchar@qq.com>
2023-09-09 23:06:15 +0800

    引入VueRouter

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createRouter, createWebHashHistory } from "vue-router";
+import "./style.css";
+import App from "./App.vue";
+import Home from "./views/Home.vue";
+import About from "./views/About.vue";
 
-createApp(App).mount('#app')
+const routes = [
+  { path: "/", component: Home },
+  { path: "/about", component: About },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
